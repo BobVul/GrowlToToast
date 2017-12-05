a8d976176d66c3dd4a304d4af56c91419a1e4c41
Bob <bob+git@vulpin.com>
2017-12-05 11:08:44 +1100

    Add WixSharp project

---
+++ WixSharp Setup/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using WixSharp;
+
+namespace WixSharp_Setup
+{
+    class Program
+    {
+        static void Main()
+        {
+            DON'T FORGET to add NuGet package "WixSharp".
+
+            var project = new Project("MyProduct",
+                              new Dir(@"%ProgramFiles%\My Company\My Product",
+                                  new File("Program.cs")));
+
+            project.GUID = new Guid("6fe30b47-2577-43ad-9095-1861ba25889b");
+            //project.SourceBaseDir = "<input dir path>";
+            //project.OutDir = "<output dir path>";
+
+            project.BuildMsi();
+        }
+    }
+}
