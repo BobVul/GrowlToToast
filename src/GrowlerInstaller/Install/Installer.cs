6dbdf4ecf01992bec53e0dfe1f5d3f2883398e74
Bob <bob+git@vulpin.com>
2017-12-15 12:22:54 +1100

    Update installer implementation

--- src/GrowlerInstaller/Install/Installer.cs
+++ src/GrowlerInstaller/Install/Installer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,21 +9,48 @@ namespace GrowlerInstaller.Install
 {
     public class Installer
     {
-        Installation details;
+        Installation source;
+        Installation target;
 
-        public Installer(Installation details)
+        public Installer(Installation source, Installation target)
         {
-            this.details = details;
+            this.source = source;
+            this.target = target;
         }
 
         public void Install()
         {
-
+            CopyDirectory(new DirectoryInfo(source.InstallPath), new DirectoryInfo(target.InstallPath), true, true);
         }
 
         public void Remove()
         {
+            Directory.Delete(target.InstallPath, true);
+        }
 
+        /// <summary>
+        /// Copy a directory specified by the source path to the target path.
+        /// </summary>
+        /// <param name="source">The Directory to copy from</param>
+        /// <param name="target">The Directory to copy to</param>
+        /// <param name="subdirs">Include subdirectories (recursive)</param>
+        /// <param name="overwrite">Overwrite existing files</param>
+        private static void CopyDirectory(DirectoryInfo source, DirectoryInfo target, bool subdirs, bool overwrite)
+        {
+            target.Create();
+
+            foreach (var file in source.EnumerateFiles())
+            {
+                file.CopyTo(Path.Combine(target.FullName, file.Name), overwrite);
+            }
+
+            if (subdirs)
+            {
+                foreach (var dir in source.EnumerateDirectories())
+                {
+                    CopyDirectory(dir, new DirectoryInfo(Path.Combine(target.FullName, dir.Name)), subdirs, overwrite);
+                }
+            }
         }
     }
 }
