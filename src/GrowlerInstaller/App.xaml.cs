c2f413d19db4b5dc601d0e79e75e55ac455fefc4
Bob <bob+git@vulpin.com>
2018-01-30 18:02:30 +1100

    Add exception handling

--- src/GrowlerInstaller/App.xaml.cs
+++ src/GrowlerInstaller/App.xaml.cs
@@ -13,5 +13,24 @@ namespace GrowlToToast.GrowlerInstaller
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (MessageBox.Show("An unhandled exception occurred: " + e.Exception.Message + "\r\n\r\nWould you like to report this error to the developer?", "An unhandled exception occurred", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                var title = "Exception: " + e.Exception.Message;
+                var body = 
+@"Describe what you were doing when this happened. Please include any data entered to make it easier to reproduce and fix.
+
+
+---
+
+```
+" + e.Exception.StackTrace + @"
+```";
+                System.Diagnostics.Process.Start($"https://github.com/BobVul/GrowlToToast/issues/new?title={System.Net.WebUtility.UrlEncode(title)}&body={System.Net.WebUtility.UrlEncode(body)}");
+            }
+            e.Handled = true;
+            Application.Current.Shutdown();
+        }
     }
 }
