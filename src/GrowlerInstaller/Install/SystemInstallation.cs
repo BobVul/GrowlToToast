419ca999c56a84178f49c13092c03eee3ffd04d9
Bob <bob+git@vulpin.com>
2018-01-31 11:41:51 +1100

    Use correct Growl paths
    Fixes #17

--- src/GrowlerInstaller/Install/SystemInstallation.cs
+++ src/GrowlerInstaller/Install/SystemInstallation.cs
@@ -10,16 +10,10 @@ namespace GrowlToToast.GrowlerInstaller.Install
     class SystemInstallation : Installation
     {
         public override string Name { get; } = "All users";
-        public override bool Installable { get; } = false;
 
-        public SystemInstallation()
-        {
-            var detector = new Growl.CoreLibrary.Detector();
-            if (detector.IsInstalled)
-            {
-                Installable = true;
-                InstallPath = Path.Combine(detector.DisplaysFolder, Constants.DefaultInstallDir);
-            }
-        }
+        // https://github.com/briandunnington/growl-for-windows/blob/6a4ba006eae20fd9b454b4b6d0fbd1cb87bb279b/Growl/Growl/_source/Utility.cs#L48
+        private static string pluginroot = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+        public override string InstallPath { get; set; } = Path.Combine(pluginroot, @"Growl\2.0.0.0\Displays", Constants.DefaultInstallDir);
+
     }
 }
--- src/GrowlerInstaller/Install/UserInstallation.cs
+++ src/GrowlerInstaller/Install/UserInstallation.cs
@@ -10,6 +10,9 @@ namespace GrowlToToast.GrowlerInstaller.Install
     class UserInstallation : Installation
     {
         public override string Name { get; } = "Current user";
-        public override string InstallPath { get; set; } = Path.Combine(Environment.ExpandEnvironmentVariables(@"%LocalAppData%\Growl\2.0.0.0\Displays"), Constants.DefaultInstallDir);
+
+        // https://github.com/briandunnington/growl-for-windows/blob/6a4ba006eae20fd9b454b4b6d0fbd1cb87bb279b/Growl/Growl/_source/Utility.cs#L47
+        private static string pluginroot = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        public override string InstallPath { get; set; } = Path.Combine(pluginroot, @"Growl\2.0.0.0\Displays", Constants.DefaultInstallDir);
     }
 }
