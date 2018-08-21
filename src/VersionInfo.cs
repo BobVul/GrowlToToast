72697e9d4e6a663bde9abf5713b2aeed53fcd7d4
Bob <bob+git@vulpin.com>
2018-08-21 13:46:00 +1000

    Bump version to 2.0.3

--- src/VersionInfo.cs
+++ src/VersionInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("2.0.2")]
+[assembly: AssemblyVersion("2.0.3.0")]
+[assembly: AssemblyFileVersion("2.0.3.0")]
+[assembly: AssemblyInformationalVersion("2.0.3")]
